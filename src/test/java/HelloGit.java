39fb7adf4a7efbed8b31cb97317f0d9483e71023
詹志龙 <zhanzhilong@chuanglintech.com>
2020-12-03 11:14:59 +0800

    First commit all

---
+++ src/test/java/HelloGit.java
@@ -0,0 +1,10 @@
+/**
+ * @ClassName:Hello
+ * @Date:2020/12/3
+ * @Author:zhanzhilong
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git, I come from IDEA.");
+    }
+}
